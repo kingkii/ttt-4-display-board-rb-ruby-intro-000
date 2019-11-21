426e15fbbaef5b9a250264561e3344af93d8df0a
Olaoluwakitan Fasuyi <kfasuyi@gmail.com>
2019-11-21 01:05:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ def display_board(board)
 
   puts " #{board[0]} |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | #{board[4]} |   "
   puts "-----------"
   puts "   |   |   "
 end
